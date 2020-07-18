5b3537d768e4495c04154a57b92df8a6ae9d9b61
Kiri Um <kirirotha@gmail.com>
2020-07-18 00:42:04 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
 
+class CashRegister
+  attr_accessor :total, :discount, :quantity
+  def initialize(total = 0)
+    @total = total
+    @discount = 20
+  end
+
+  def total
+    self.total
+  end
+
+  def add_item(title, price, quantity=1)
+    self.total += (price*quantity)
+  end
+
+  def apply_discount
+    self.total = (@total.to_f) * (100-@discount) * 0.01
+    "After the discount, the total comes to $800."
+  end
+
+
+
+
+
+
+
+
+end
