ad0272fe4b962615ae1ff76bdbcb3552dacaa756
Kiri Um <kirirotha@gmail.com>
2020-07-18 00:42:55 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,18 +6,7 @@ class CashRegister
     @discount = 20
   end
 
-  def total
-    self.total
-  end
-
-  def add_item(title, price, quantity=1)
-    self.total += (price*quantity)
-  end
-
-  def apply_discount
-    self.total = (@total.to_f) * (100-@discount) * 0.01
-    "After the discount, the total comes to $800."
-  end
+  
 
 
 
